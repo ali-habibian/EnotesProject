c1517bf90ab4b5c19dbeda1fa1db09525abd93d0
Ali Habibian <ali.habibian04@gmail.com>
2021-10-07 14:20:04 +0330

    added UserDao.java

--- src/main/java/io/github/alihabibian/enotsproject/dao/UserDao.java
+++ src/main/java/io/github/alihabibian/enotsproject/dao/UserDao.java
@@ -1,5 +1,36 @@
 package io.github.alihabibian.enotsproject.dao;
 
-public class UserDao {
+import io.github.alihabibian.enotsproject.user.UserDetails;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserDao {
+    private Connection conn;
+
+    public UserDao(Connection conn) {
+        super();
+        this.conn = conn;
+    }
+
+    public boolean addUser(UserDetails userDetails) {
+        boolean isAdded = false;
+        //language=SQL
+        String query = "INSERT INTO `e_notes`.`user` () VALUES (?,?,?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, userDetails.getName());
+            ps.setString(2, userDetails.getEmail());
+            ps.setString(3, userDetails.getPassword());
+
+            int result = ps.executeUpdate();
+
+            if (result == 1)
+                isAdded = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isAdded;
+    }
 }
