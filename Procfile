ed5176338c738604a939918cecd2d2fd3d38b8cc
Ali Habibian <ali.habibian04@gmail.com>
2021-10-23 00:44:31 +0330

    Configured for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp target/classes:target/dependency/* io.github.ali-habibian.EnotsProject
+ $ java -jar target/dependency/webapp-runner.jar --port $PORT target/*.war
--- src/main/java/io/github/alihabibian/enotsproject/db/DbConnect.java
+++ src/main/java/io/github/alihabibian/enotsproject/db/DbConnect.java
@@ -10,8 +10,14 @@ public class DbConnect {
     public static Connection getConn() {
         if (conn == null) {
             try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/e_notes", "root", "");
+//                For mysql
+//                Class.forName("com.mysql.cj.jdbc.Driver");
+//                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/e_notes", "root", "");
+
+                // For postgresql
+                Class.forName("org.postgresql.Driver");
+                conn = DriverManager.getConnection("jdbc:postgresql://ec2-23-23-164-251.compute-1.amazonaws.com:5432/df4q0mvvm9d3hd?sslmode=require",
+                        "kakwiasmqhsoro", "245b28c84adfcd661efe6d64d6c71e130a9f30f117e70ba6bd2c8c3322b56d8c");
             } catch (SQLException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
